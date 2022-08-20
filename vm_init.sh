3daf55a98b8a8d64e7e218a3e12c96a4459b6361
кирилл бондарь <ironbondar@gmail.com>
2022-08-20 15:23:31 +0300

    vm init script

---
+++ vm_init.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install nano
+sudo apt install git
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+sudo groupadd docker
+sudo usermod -aG docker kirill
+echo "ТЕПЕРЬ НАДО ВЫЙТИ И ЗАЙТИ НА МАШИНУ СНОВА"
